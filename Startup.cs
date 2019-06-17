2e20c40267115346670f2847195d555142ba5fe7
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-06-17 13:27:53 -0400

    migration to Preview 6 & prototyping

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using BlazePort.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ namespace BlazePort
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped<LocationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,7 @@ namespace BlazePort
             }
             else
             {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
