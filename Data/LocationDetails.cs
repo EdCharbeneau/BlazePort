163ffacee0c6fc8de7e60541d9da3dc6c0897170
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-06-19 13:16:00 -0400

    - Fixed masonry layout - Added detail page - using Genfu for data

--- Data/LocationDetails.cs
+++ Data/LocationDetails.cs
@@ -10,6 +10,7 @@ namespace BlazePort.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImageUrl { get; set; }
 
         public void Deconstruct(out int id, out string name)
         {
