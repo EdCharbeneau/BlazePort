c59f09949e8ce8e70c4f2233308a4bcc5a9cbd7d
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-06-27 11:52:48 -0400

    moved git root location

---
+++ BlazePort/Data/LocationDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazePort.Data
+{
+    public class LocationDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public List<PortDetails> Ports { get; set; }
+
+        public void Deconstruct(out int id, out string name)
+        {
+            id = Id;
+            name = Name;
+        }
+        public void Deconstruct(out int id, out string name, out string description)
+        {
+            id = Id;
+            name = Name;
+            description = Description;
+        }
+
+    }
+}
