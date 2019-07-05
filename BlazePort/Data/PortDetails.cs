111972eb2208bc90c146dd68fac9ae3a7498be3d
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-07-05 13:52:31 -0400

    Added form for data binding. Completed Port details form.

--- BlazePort/Data/PortDetails.cs
+++ BlazePort/Data/PortDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace BlazePort.Data
         public double Lat { get; set; }
         public double Long { get; set; }
 
+        [Required]
         public int LocationId { get; set; }
 
         public LocationDetails Location { get; set; }
---
+++ BlazePort/Models/FormModels/PortDetailsForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazePort.Models.FormModels
+{
+    public class PortDetailsForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public string ImageUrl { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string SelectedLocation { get; set; }
+    }
+}
