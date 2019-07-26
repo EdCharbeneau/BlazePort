3ee35b567750dc5a0e319de901b8728f513cc0d2
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-07-26 14:06:40 -0400

    Added Telerik UI for blazor Upgraded Admin pages with grids

---
+++ BlazePort/Models/ViewModels/PortDetailsGridView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazePort.Models.ViewModels
+{
+    public class PortDetailsGridView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string ShortDescriptioin => String.Join("",Description.Take(160)) + "...";
+        public string ImageUrl { get; set; }
+        public string Country { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+
+        public string LatLong => $"{Lat}, {Long}";
+
+        public string LocationName { get; set; }
+    }
+}
