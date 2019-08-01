412498dad75b915ac599d89d0de75285381e2f96
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-08-01 16:27:23 -0400

    Changed from Models approach to feature slices.

--- BlazePort/Pages/Admin/Ports/PortDetailsGridView.cs
+++ BlazePort/Pages/Admin/Ports/PortDetailsGridView.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazePort.Models.ViewModels
+namespace BlazePort.Pages.Admin
 {
     public class PortDetailsGridView
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
         public string ShortDescriptioin => String.Join("",Description.Take(160)) + "...";
         public string ImageUrl { get; set; }
         public string Country { get; set; }
