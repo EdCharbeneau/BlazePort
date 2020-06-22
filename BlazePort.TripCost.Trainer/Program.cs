b3485c9c657cbf1254de0fa28f59c1071c766f4f
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-06-22 16:03:47 -0400

    Updated to .NET Core 3.1

--- BlazePort.TripCost.Trainer/Program.cs
+++ BlazePort.TripCost.Trainer/Program.cs
@@ -36,7 +36,8 @@ namespace BlazePort.TripCost.Trainer
             TestSinglePrediction();
 
             // Paint regression distribution chart for a number of elements read from a Test DataSet file
-            PlotRegressionChart(mlContext, TestDataPath, 100, args);
+            // TODO: Remove PLplot and replace with something more stable https://github.com/surban/PLplotNet/issues/2
+            // PlotRegressionChart(mlContext, TestDataPath, 100, args);
 
             Console.WriteLine("Press any key to exit..");
             Console.ReadLine();
@@ -119,165 +120,165 @@ namespace BlazePort.TripCost.Trainer
             Console.WriteLine($"**********************************************************************");
         }
 
-        private static void PlotRegressionChart(MLContext mlContext,
-                                                string testDataSetPath,
-                                                int numberOfRecordsToRead,
-                                                string[] args)
-        {
-            ITransformer trainedModel;
-            DataViewSchema modelSchema;
-            using (var stream = new FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                trainedModel = mlContext.Model.Load(stream, out modelSchema);
-            }
+        //private static void PlotRegressionChart(MLContext mlContext,
+        //                                        string testDataSetPath,
+        //                                        int numberOfRecordsToRead,
+        //                                        string[] args)
+        //{
+        //    ITransformer trainedModel;
+        //    DataViewSchema modelSchema;
+        //    using (var stream = new FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+        //    {
+        //        trainedModel = mlContext.Model.Load(stream, out modelSchema);
+        //    }
 
-            // Create prediction engine related to the loaded trained model
-            var predFunction = mlContext.Model.CreatePredictionEngine<Trip, TripCostPrediction>(trainedModel);
+        //    // Create prediction engine related to the loaded trained model
+        //    var predFunction = mlContext.Model.CreatePredictionEngine<Trip, TripCostPrediction>(trainedModel);
 
-            string chartFileName = "";
-            using (var pl = new PLStream())
-            {
-                // use SVG backend and write to SineWaves.svg in current directory
-                if (args.Length == 1 && args[0] == "svg")
-                {
-                    pl.sdev("svg");
-                    chartFileName = "TaxiRegressionDistribution.svg";
-                    pl.sfnam(chartFileName);
-                }
-                else
-                {
-                    pl.sdev("pngcairo");
-                    chartFileName = "TaxiRegressionDistribution.png";
-                    pl.sfnam(chartFileName);
-                }
+        //    string chartFileName = "";
+        //    using (var pl = new PLStream())
+        //    {
+        //        // use SVG backend and write to SineWaves.svg in current directory
+        //        if (args.Length == 1 && args[0] == "svg")
+        //        {
+        //            pl.sdev("svg");
+        //            chartFileName = "TaxiRegressionDistribution.svg";
+        //            pl.sfnam(chartFileName);
+        //        }
+        //        else
+        //        {
+        //            pl.sdev("pngcairo");
+        //            chartFileName = "TaxiRegressionDistribution.png";
+        //            pl.sfnam(chartFileName);
+        //        }
 
-                // use white background with black foreground
-                pl.spal0("cmap0_alternate.pal");
+        //        // use white background with black foreground
+        //        pl.spal0("cmap0_alternate.pal");
 
-                // Initialize plplot
-                pl.init();
+        //        // Initialize plplot
+        //        pl.init();
 
-                // set axis limits
-                const int xMinLimit = 0;
-                const int xMaxLimit = 35; //Rides larger than $35 are not shown in the chart
-                const int yMinLimit = 0;
-                const int yMaxLimit = 35;  //Rides larger than $35 are not shown in the chart
-                pl.env(xMinLimit, xMaxLimit, yMinLimit, yMaxLimit, AxesScale.Independent, AxisBox.BoxTicksLabelsAxes);
+        //        // set axis limits
+        //        const int xMinLimit = 0;
+        //        const int xMaxLimit = 35; //Rides larger than $35 are not shown in the chart
+        //        const int yMinLimit = 0;
+        //        const int yMaxLimit = 35;  //Rides larger than $35 are not shown in the chart
+        //        pl.env(xMinLimit, xMaxLimit, yMinLimit, yMaxLimit, AxesScale.Independent, AxisBox.BoxTicksLabelsAxes);
 
-                // Set scaling for mail title text 125% size of default
-                pl.schr(0, 1.25);
+        //        // Set scaling for mail title text 125% size of default
+        //        pl.schr(0, 1.25);
 
-                // The main title
-                pl.lab("Measured", "Predicted", "Distribution of Taxi Fare Prediction");
+        //        // The main title
+        //        pl.lab("Measured", "Predicted", "Distribution of Taxi Fare Prediction");
 
-                // plot using different colors
-                // see http://plplot.sourceforge.net/examples.php?demo=02 for palette indices
-                pl.col0(1);
+        //        // plot using different colors
+        //        // see http://plplot.sourceforge.net/examples.php?demo=02 for palette indices
+        //        pl.col0(1);
 
-                int totalNumber = numberOfRecordsToRead;
-                var testData = new TripCsvReader().GetDataFromCsv(testDataSetPath, totalNumber).ToList();
+        //        int totalNumber = numberOfRecordsToRead;
+        //        var testData = new TripCsvReader().GetDataFromCsv(testDataSetPath, totalNumber).ToList();
 
-                //This code is the symbol to paint
-                char code = (char)9;
+        //        //This code is the symbol to paint
+        //        char code = (char)9;
 
-                // plot using other color
-                //pl.col0(9); //Light Green
-                //pl.col0(4); //Red
-                pl.col0(2); //Blue
+        //        // plot using other color
+        //        //pl.col0(9); //Light Green
+        //        //pl.col0(4); //Red
+        //        pl.col0(2); //Blue
 
-                double yTotal = 0;
-                double xTotal = 0;
-                double xyMultiTotal = 0;
-                double xSquareTotal = 0;
+        //        double yTotal = 0;
+        //        double xTotal = 0;
+        //        double xyMultiTotal = 0;
+        //        double xSquareTotal = 0;
 
-                for (int i = 0; i < testData.Count; i++)
-                {
-                    var x = new double[1];
-                    var y = new double[1];
+        //        for (int i = 0; i < testData.Count; i++)
+        //        {
+        //            var x = new double[1];
+        //            var y = new double[1];
 
-                    //Make Prediction
-                    var FarePrediction = predFunction.Predict(testData[i]);
+        //            //Make Prediction
+        //            var FarePrediction = predFunction.Predict(testData[i]);
 
-                    x[0] = testData[i].FareAmount;
-                    y[0] = FarePrediction.FareAmount;
+        //            x[0] = testData[i].FareAmount;
+        //            y[0] = FarePrediction.FareAmount;
 
-                    //Paint a dot
-                    pl.poin(x, y, code);
+        //            //Paint a dot
+        //            pl.poin(x, y, code);
 
-                    xTotal += x[0];
-                    yTotal += y[0];
+        //            xTotal += x[0];
+        //            yTotal += y[0];
 
-                    double multi = x[0] * y[0];
-                    xyMultiTotal += multi;
+        //            double multi = x[0] * y[0];
+        //            xyMultiTotal += multi;
 
-                    double xSquare = x[0] * x[0];
-                    xSquareTotal += xSquare;
+        //            double xSquare = x[0] * x[0];
+        //            xSquareTotal += xSquare;
 
-                    double ySquare = y[0] * y[0];
+        //            double ySquare = y[0] * y[0];
 
-                    Console.WriteLine($"-------------------------------------------------");
-                    Console.WriteLine($"Predicted : {FarePrediction.FareAmount}");
-                    Console.WriteLine($"Actual:    {testData[i].FareAmount}");
-                    Console.WriteLine($"- Distance:    {testData[i].TripDistance}");
-                    Console.WriteLine($"- Payment:    {testData[i].PaymentType}");
-                    Console.WriteLine($"- Passengers:    {testData[i].PassengerCount}");
-                    Console.WriteLine($"- Rate Code:    {testData[i].RateCode}");
-                    Console.WriteLine($"-------------------------------------------------");
-                }
+        //            Console.WriteLine($"-------------------------------------------------");
+        //            Console.WriteLine($"Predicted : {FarePrediction.FareAmount}");
+        //            Console.WriteLine($"Actual:    {testData[i].FareAmount}");
+        //            Console.WriteLine($"- Distance:    {testData[i].TripDistance}");
+        //            Console.WriteLine($"- Payment:    {testData[i].PaymentType}");
+        //            Console.WriteLine($"- Passengers:    {testData[i].PassengerCount}");
+        //            Console.WriteLine($"- Rate Code:    {testData[i].RateCode}");
+        //            Console.WriteLine($"-------------------------------------------------");
+        //        }
 
-                // Regression Line calculation explanation:
-                // https://www.khanacademy.org/math/statistics-probability/describing-relationships-quantitative-data/more-on-regression/v/regression-line-example
+        //        // Regression Line calculation explanation:
+        //        // https://www.khanacademy.org/math/statistics-probability/describing-relationships-quantitative-data/more-on-regression/v/regression-line-example
 
-                double minY = yTotal / totalNumber;
-                double minX = xTotal / totalNumber;
-                double minXY = xyMultiTotal / totalNumber;
-                double minXsquare = xSquareTotal / totalNumber;
+        //        double minY = yTotal / totalNumber;
+        //        double minX = xTotal / totalNumber;
+        //        double minXY = xyMultiTotal / totalNumber;
+        //        double minXsquare = xSquareTotal / totalNumber;
 
-                double m = ((minX * minY) - minXY) / ((minX * minX) - minXsquare);
+        //        double m = ((minX * minY) - minXY) / ((minX * minX) - minXsquare);
 
-                double b = minY - (m * minX);
+        //        double b = minY - (m * minX);
 
-                //Generic function for Y for the regression line
-                // y = (m * x) + b;
+        //        //Generic function for Y for the regression line
+        //        // y = (m * x) + b;
 
-                double x1 = 1;
-                //Function for Y1 in the line
-                double y1 = (m * x1) + b;
+        //        double x1 = 1;
+        //        //Function for Y1 in the line
+        //        double y1 = (m * x1) + b;
 
-                double x2 = 39;
-                //Function for Y2 in the line
-                double y2 = (m * x2) + b;
+        //        double x2 = 39;
+        //        //Function for Y2 in the line
+        //        double y2 = (m * x2) + b;
 
-                var xArray = new double[2];
-                var yArray = new double[2];
-                xArray[0] = x1;
-                yArray[0] = y1;
-                xArray[1] = x2;
-                yArray[1] = y2;
+        //        var xArray = new double[2];
+        //        var yArray = new double[2];
+        //        xArray[0] = x1;
+        //        yArray[0] = y1;
+        //        xArray[1] = x2;
+        //        yArray[1] = y2;
 
-                pl.col0(4);
-                pl.line(xArray, yArray);
+        //        pl.col0(4);
+        //        pl.line(xArray, yArray);
 
-                // end page (writes output to disk)
-                pl.eop();
+        //        // end page (writes output to disk)
+        //        pl.eop();
 
-                // output version of PLplot
-                pl.gver(out var verText);
-                Console.WriteLine("PLplot version " + verText);
+        //        // output version of PLplot
+        //        pl.gver(out var verText);
+        //        Console.WriteLine("PLplot version " + verText);
 
-            } // the pl object is disposed here
+        //    } // the pl object is disposed here
 
-            // Open Chart File In Microsoft Photos App (Or default app, like browser for .svg)
+        //    // Open Chart File In Microsoft Photos App (Or default app, like browser for .svg)
 
-            Console.WriteLine("Showing chart...");
-            var p = new Process();
-            string chartFileNamePath = @".\" + chartFileName;
-            p.StartInfo = new ProcessStartInfo(chartFileNamePath)
-            {
-                UseShellExecute = true
-            };
-            p.Start();
-        }
+        //    Console.WriteLine("Showing chart...");
+        //    var p = new Process();
+        //    string chartFileNamePath = @".\" + chartFileName;
+        //    p.StartInfo = new ProcessStartInfo(chartFileNamePath)
+        //    {
+        //        UseShellExecute = true
+        //    };
+        //    p.Start();
+        //}
 
         public static string GetAbsolutePath(string relativePath)
         {
